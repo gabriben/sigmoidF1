c914b2d25787d41c6094d7516f3debe54beca1d6
Gabriel Bénédict <gbndict@gmail.com>
2021-09-27 10:27:20 +0200

    set rank to zero

--- train.py
+++ train.py
@@ -145,7 +145,7 @@ def main( data = '/dbfs/datasets/coco', model_file_name = "tresnet_m_21K", ep = 
 
     # parallel
     if torch.cuda.device_count() > 1:
-        # torch.cuda.set_device(0)
+        torch.cuda.set_device(0)
         if not torch.distributed.is_initialized():
             torch.distributed.init_process_group(backend='nccl', init_method='env://')
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[0,1,2,3])
